ed95993802e52b89e9cba5ecda280f96c13c507a
ekgml3765 <ekgml3765@naver.com>
2020-08-11 22:08:20 +0900

    0811_미완성

---
+++ hw_0811/src/hw_0811/Refrigerator.java
@@ -0,0 +1,14 @@
+package hw_0811;
+
+public class Refrigerator extends Product {
+
+	Refrigerator(int p_Num, String p_Name, int p_price, String info, int amount, int capacity) {
+		super.setP_Num(p_Num);
+		super.setP_Name(p_Name);
+		super.setP_price(p_price);
+		super.setInfo(info);
+		super.setAmount(amount);
+		super.setCapacity(capacity);
+	}
+
+}
