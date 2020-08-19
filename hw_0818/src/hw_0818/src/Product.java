d24a094833708a260ec31cae63435639f58b4ed7
HwangDahui <HwangDahui@DESKTOP-KB8785B>
2020-08-19 19:29:16 +0900

    0818

---
+++ hw_0818/src/hw_0818/src/Product.java
@@ -0,0 +1,69 @@
+package hw_0818.src;
+import java.io.Serializable;
+
+public class Product implements Serializable{
+
+	private int p_Num, p_price, capacity, amount;
+	private String p_Name, info;
+
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+
+	public int getP_Num() {
+		return p_Num;
+	}
+
+	public int getP_price() {
+		return p_price;
+	}
+
+	public void setP_price(int p_price) {
+		this.p_price = p_price;
+	}
+
+	public void setP_Num(int p_Num) {
+		this.p_Num = p_Num;
+	}
+
+	public String getP_Name() {
+		return p_Name;
+	}
+
+	public void setP_Name(String p_Name) {
+		this.p_Name = p_Name;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [p_Num=" + p_Num + ", p_price=" + p_price + ", capacity=" + capacity + ", amount=" + amount
+				+ ", p_Name=" + p_Name + ", info=" + info + "]";
+	}
+
+
+
+	
+	
+	
+}
--- hw_0818/src/hw_0818/src/ProductTest.java
+++ hw_0818/src/hw_0818/src/ProductTest.java
@@ -1,4 +1,4 @@
-package hw_0811;
+package hw_0818.src;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -10,10 +10,8 @@ public class ProductTest {
 
 		Scanner sc = new Scanner(System.in);
 		boolean flag = true;
-		
 
 		ProductMgrImpl pdmgr = ProductMgrImpl.getInstance();
-		//스레드 처리
 
 		while (flag) {
 
@@ -28,6 +26,7 @@ public class ProductTest {
 			System.out.println("8. 상품번호로 삭제");
 			System.out.println("9. 상품 가격 변경하기");
 			System.out.println("10. ~이상의 TV 또는 냉장고 검색");
+			System.out.println("11. 저장하기");
 			System.out.println("0. 종료");
 			System.out.println("==============원하는 번호를 입력하시오:");
 			int n = sc.nextInt();
@@ -55,11 +54,23 @@ public class ProductTest {
 				int capacity = sc.nextInt();
 				if (info.equals("TV")) {
 					TV tv = new TV(p_num, p_Name, price, info, amount, capacity);
-					pdmgr.add(tv);
+					try {
+						pdmgr.add(tv);
+						//저장하자
+						//pdmgr.save();
+					} catch (DuplicateException e) {
+						System.out.println("상품 번호가 같으며 이미 존재하는 상품입니다.");
+					}
 				}
 				if (info.equals("냉장고")) {
 					Refrigerator re = new Refrigerator(p_num, p_Name, price, info, amount, capacity);
-					pdmgr.add(re);
+					try {
+						pdmgr.add(re);
+						//pdmgr.save();
+					} catch (DuplicateException e) {
+						System.out.println("상품 번호가 같으며 이미 존재하는 상품입니다.");
+
+					}
 				}
 				break;
 			}
@@ -67,18 +78,22 @@ public class ProductTest {
 			case 2: {// 상품 정보 전체 검색
 				ArrayList<Product> list = new ArrayList<>();
 				list = pdmgr.serch();
-				for (int i = 0; i < list.size(); i++)
-					System.out.println(list.get(i).toString());
+				for (int i = 0; i < pdmgr.getSize(); i++)
+					System.out.println(list.get(i));
 				break;
 			}
 
 			case 3: {// 상품 번호검색
 				System.out.println("상품 번호를 입력해주세요:");
 				int number = sc.nextInt();
-				for ( Product x : pdmgr.serch_Num(number)) {
-					if (x != null) {
-						System.out.println(x);
+				try {
+					for (Product x : pdmgr.serch_Num(number)) {
+						if (x != null) {
+							System.out.println(x);
+						}
 					}
+				} catch (CodeNotFoundException e) {
+					System.out.println("상품번호가 존재하지 않습니다");
 				}
 				break;
 			}
@@ -127,7 +142,7 @@ public class ProductTest {
 				break;
 			}
 
-			case 9: {//상품가격 변경
+			case 9: {// 상품가격 변경
 				System.out.println("상품 번호를 입력하세요:");
 				int num = sc.nextInt();
 				System.out.println("변경할 가격을 입력하세요:");
@@ -136,19 +151,28 @@ public class ProductTest {
 				System.out.println("변경되었습니다.");
 				break;
 			}
-			
+
 			case 10: {
 				System.out.println("무엇을 검색하시겠습니까? TV or 냉장고 ");
 				String s = sc.next();
 				System.out.println("용량 크기를 숫자로만 입력하세요.");
 				int size = sc.nextInt();
-				for (Product x : pdmgr.serch_Info2(s, size)) {
-					if (x != null) {
-						System.out.println(x);
+				try {
+					for (Product x : pdmgr.serch_Info2(s, size)) {
+						if (x != null) {
+							System.out.println(x);
+						}
 					}
+				} catch (ProductNotFoundException e) {
+					System.out.println("그런 상품은 존재하지 않습니다.");
 				}
 				break;
 			}
+			
+			case 11: {
+				pdmgr.save();
+			}
+			
 
 			default:
 				break;
